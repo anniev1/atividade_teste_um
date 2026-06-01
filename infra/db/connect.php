e454a5a6987958415a5fe61863e9389da023d3d3
annie <annie_marie@estudante.sesisenai.org.br>
2026-06-01 12:58:18 -0300

    add - connectveri

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -5,10 +5,15 @@
     $pass = "root";
     $db = "sistema_simples_m1";
 
+
+    //faz conexão com o banco de dados
     $conn = new mysqli($host,$user,$pass,$db);
 
+
+    //verifica se conecto
     if($conn->connect_error){
         die("Erro na conexão!");
+
     }else{
         echo "<script>console.log('Banco conectado com sucesso!')</script>";
     };
